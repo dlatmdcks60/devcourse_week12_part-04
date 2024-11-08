5c937b2b5581c1b0d6984c4a29786ad0a8e5c88a
SeungChan <dlatmdcks60@gmail.com>
2024-11-08 12:08:23 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+const finalSpaceCharacters = [
+    {
+        id: 'gary',
+        name: 'Gary Goodspeed',
+    },
+    {
+        id: 'cato',
+        name: 'Little Cato',
+    },
+    {
+        id: 'kvn',
+        name: 'KVN',
+    },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [characters, setCharacters] = useState(finalSpaceCharacters);
+    const handleEnd = (result) => {
+        if (!result.destination) return;
+
+        const items = Array.from(characters);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+        setCharacters(items);
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Final Space Characters</h1>
+                <DragDropContext onDragEnd={handleEnd}>
+                    <Droppable droppableId="characters">
+                        {(provided) => (
+                            <ul className="characters" {...provided.droppableProps} ref={provided.innerRef}>
+                                {characters.map(({ id, name }, index) => {
+                                    return (
+                                        <Draggable key={id} draggableId={id} index={index}>
+                                            {(provided) => (
+                                                <li ref={provided.innerRef} {...provided.droppableProps} {...provided.dragHandleProps}>
+                                                    <p>{name}</p>
+                                                </li>
+                                            )}
+                                        </Draggable>
+                                    );
+                                })}
+                                {provided.placeholder}
+                            </ul>
+                        )}
+                    </Droppable>
+                </DragDropContext>
+            </header>
+        </div>
+    );
 }
 
 export default App;
